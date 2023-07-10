db6ace1d2dc88dda95bc4628e946ba23a78e328f
Ahayaat <amala.hayat55@gmail.com>
2023-07-10 22:26:52 +0500

    add commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,19 @@
+import Footer from "@/views/Footer";
 import Hero from "@/views/Hero";
+import Jewellery from "@/views/Jewellery";
+import Newslatter from "@/views/Newslatter";
 import ProductList from "@/views/ProductList";
 import ProductType from "@/views/ProductType";
-import BASE_PATH_FORAPI from "@/components/BasePath";
-import ProductCarousel from "@/views/ProductCarousel";
-
-// async function fetchAllProductsData() {
-//   let res = await fetch(`${BASE_PATH_FORAPI}/api/products`);
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch");
-//   }
-
-//   return res.json();
-// }
 
 export default async function Home() {
-  // let { result } = await fetchAllProductsData();
   return (
     <div>
       <Hero />
       <ProductType />
-      {/* <ProductCarousel ProductData={result} /> */}
       <ProductList />
+      <Jewellery />
+      <Newslatter />
+      <Footer />
     </div>
   );
 }
--- src/components/BasePath.ts
+++
@@ -1,3 +0,0 @@
-const BASE_PATH_FORAPI = "http://localhost:3000";
-
-export default BASE_PATH_FORAPI;
